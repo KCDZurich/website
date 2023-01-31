d5cc619b591c9bea7e5bc75a2f0f8265b0b78a02
Den-Bratchikov <denbratch@gmail.com>
2023-01-31 12:58:43 +0500

    chore: add Information section for Workshop-argo page

--- src/pages/workshop-argo.jsx
+++ src/pages/workshop-argo.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 
 import Hero from 'components/pages/argo-workshop/hero';
+import Information from 'components/pages/argo-workshop/information';
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
 
 const WorkshopArgo = () => (
   <Layout>
     <Hero />
+    <Information />
   </Layout>
 );
 
