7d041962a13ce9bed00777afb33e58a1b404847b
Raman Yasel <raman.yasel@gmail.com>
2023-06-20 17:39:52 +0300

    feat: add archive component

--- src/pages/archive.jsx
+++ src/pages/archive.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
+import Archive from 'components/pages/archive/archive';
 import Hero from 'components/pages/archive/hero';
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
@@ -9,6 +10,7 @@ import SEO_DATA from 'constants/seo-data';
 const ArchivePage = () => (
   <Layout>
     <Hero />
+    <Archive />
   </Layout>
 );
 
