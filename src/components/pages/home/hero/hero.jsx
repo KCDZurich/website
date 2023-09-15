badb63a12c5dda342ec0b5afda4cc521cf199316
Raman Yasel <raman.yasel@gmail.com>
2023-09-15 10:06:20 +0300

    chore: caps home hero label

--- src/components/pages/home/hero/hero.jsx
+++ src/components/pages/home/hero/hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => (
           className="rounded-3xl bg-yellow px-4 py-2 text-sm font-bold leading-none text-black"
           dateTime="2023-06-14"
         >
-          Coming soon
+          COMING SOON
         </time>
         <h1 className="mt-3 max-w-[570px] text-8xl font-bold leading-denser text-primary-1 lg:max-w-[500px] sm:text-7xl">
           {TITLE}
