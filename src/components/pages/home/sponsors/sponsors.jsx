6d4a285d1c84c962919ac0c92c8d09ae95783e8e
Raman Yasel <raman.yasel@gmail.com>
2023-11-10 18:59:02 +0300

    feat: add tablet sponsors image

--- src/components/pages/home/sponsors/sponsors.jsx
+++ src/components/pages/home/sponsors/sponsors.jsx
@@ -8,7 +8,7 @@ const title = 'Sponsors';
 const Sponsors = () => (
   <section className="safe-paddings relative pt-[113px] lg:pt-9 md:pt-6 xs:pt-8" id="sponsors">
     <div className="container-xl bg-blue-light bg-opacity-90">
-      <div className="container flex items-center gap-x-20 py-[72px] lg:gap-x-16 lg:!px-4 lg:py-14 md:gap-x-4 md:!px-2 md:py-14 xs:flex-col-reverse xs:!px-0">
+      <div className="container flex items-center gap-x-20 py-[72px] lg:gap-x-16 lg:!px-4 lg:py-14 md:gap-x-4 md:!px-2 md:py-14 xs:flex-col-reverse xs:gap-y-4 xs:!px-0">
         <div className="text-2xl leading-normal text-primary-1 lg:text-base md:text-sm xs:text-base">
           <h2 className="font-sans-cyber text-[52px] font-bold lowercase leading-tight lg:text-[40px] md:text-[32px]">
             {title}
@@ -29,10 +29,18 @@ const Sponsors = () => (
           </Button>
         </div>
         <StaticImage
-          className="-mr-2 max-w-[560px] shrink-0 lg:mr-0 lg:max-w-[474px] md:max-w-[355px] xs:max-w-full"
+          className="-mr-2 max-w-[560px] shrink-0 lg:mr-0 lg:max-w-[474px] md:!hidden"
           src="./images/sponsors-illustration.png"
           width={549}
           heigth={349}
+          alt=""
+          loading="lazy"
+        />
+        <StaticImage
+          className="-mr-4 -mt-5 !hidden max-w-[355px] shrink-0 md:!block xs:mr-0 xs:mt-0 xs:max-w-[95%]"
+          src="./images/sponsors-tablet.png"
+          width={355}
+          heigth={221}
           alt=""
           loading="lazy"
         />
