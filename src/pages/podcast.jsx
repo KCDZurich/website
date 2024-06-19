0c43fd563e005b000d7113c416056459326782c7
Raman Yasel <raman.yasel@gmail.com>
2024-06-19 14:35:49 +0300

    feat: update podcast page

--- src/pages/podcast.jsx
+++ src/pages/podcast.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 
 import Hero from 'components/pages/podcast/hero';
+import Info from 'components/pages/podcast/info';
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
 import SEO_DATA from 'constants/seo-data';
@@ -9,6 +10,7 @@ import SEO_DATA from 'constants/seo-data';
 const Podcast = () => (
   <Layout headerClassnames="!bg-white">
     <Hero />
+    <Info />
   </Layout>
 );
 
